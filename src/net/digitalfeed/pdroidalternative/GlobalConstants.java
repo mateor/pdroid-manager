b62c234af4e4bfe0fabd49d2462388a2c63612d5
Simeon Morgan <smorgan@digitalfeed.net>
2012-12-14 08:03:11 +1100

    Added testing of whether the WRITE_PRIVACY_SETTINGS permission was present, to avoid crashes when both PDroid Manager and CollegeDev's PDroid 2.0 app are installed at the same time.

--- src/net/digitalfeed/pdroidalternative/GlobalConstants.java
+++ src/net/digitalfeed/pdroidalternative/GlobalConstants.java
@@ -11,6 +11,9 @@ public class GlobalConstants {
 	// If crash-happy is true, exceptions will be preferred over logging of errors.
 	// this is basically to make problems easier to see when testing. It should NEVER be true in releases.
 	public static final boolean CRASH_HAPPY = false; 
+	
+	public static final String WRITE_SETTINGS_PERMISSION = "android.privacy.WRITE_PRIVACY_SETTINGS";
+	public static final String READ_SETTINGS_PERMISSION = "android.privacy.READ_PRIVACY_SETTINGS";
 	private GlobalConstants() {}
 
 }
